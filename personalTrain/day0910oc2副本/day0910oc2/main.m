05818f75432f8b1162f03883d7db5a0c6896cf0f
lsgit <848949609@qq.com day06 lsgit>
2014-09-12 12:38:43 +0800

    hhhhh
    hhhh

---
+++ personalTrain/day0910oc2副本/day0910oc2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  day0910oc2
+//
+//  Created by liushuai on 14-9-10.
+//  Copyright (c) 2014年 Liushuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
